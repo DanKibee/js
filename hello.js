5620b4997d4af8175353ebafda5180c4ba5e7130
DanKibee <kibedanteh@gmail.com>
2024-05-10 16:09:09 +0300

    Used js for file handling

--- hello.js
+++ hello.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+let body=document.getElementById('body')
+let p = document.createElement('div')
+p.className = 'gallery'
+p.id='gallery'
+body.appendChild(p)
+console.log(p)
+let counter = 0;
+while (counter < 12) {
+  let div_tag = document.createElement("div");
+  p.appendChild(div_tag);
+  for (let i = counter; i < counter + 4; i++) {
+    let img_tag = document.createElement("img");
+    div_tag.appendChild(img_tag);
+    let path = `./gallery/image_${i + 1}.jpg`;
+    img_tag.src = path;
+    img_tag.alt = "picture";  
+  }
+  counter += 4;
+}
